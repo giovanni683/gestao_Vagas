7c03b89fb4bb1aca97ab43f07a2b465c713f7f31
giovanni <giovannipuppets@gmail.com>
2025-10-26 18:03:23 -0300

    create dto and security

---
+++ gestao_vagas/src/main/java/br/com/project/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,49 @@
+package br.com.project.gestao_vagas.modules.company.useCases;
+
+import javax.naming.AuthenticationException;
+
+import br.com.project.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.project.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
+                () -> {
+                    throw new UsernameNotFoundException("Username/password incorrect");
+                });
+        // Verificar se as senhas sao iguais
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+            // Se nao for igual -> Erro
+            if(!passwordMatches) {
+                throw new AuthenticationException();
+        }
+            // Se for igual -> Gerar o token
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            var token = JWT.create().withIssuer("javagas")
+                    .withSubject(company.getId().toString())
+                    .sign(algorithm);
+            return token;
+    }
+
+}
