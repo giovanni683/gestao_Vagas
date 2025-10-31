d36e1b34f628753589b257622efd6dc62fad54b3
giovanni <giovannipuppets@gmail.com>
2025-10-31 00:04:37 -0300

    recovery JWT

--- gestao_vagas/src/main/java/br/com/project/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ gestao_vagas/src/main/java/br/com/project/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -13,6 +13,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
+
 
 @Service
 public class AuthCompanyUseCase {
@@ -41,6 +44,7 @@ public class AuthCompanyUseCase {
             // Se for igual -> Gerar o token
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
             var token = JWT.create().withIssuer("javagas")
+                    .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                     .withSubject(company.getId().toString())
                     .sign(algorithm);
             return token;
